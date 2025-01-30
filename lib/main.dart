f6668ec8fdbd087dfab5382f8c17eff3fbeb937a
Suvansh Mahajan <suvanshmahajan2001@gmail.com>
2025-01-30 13:55:11 +0530

    feat: revert color change

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.blue,
       ),
       home: LoadingPage(),
     );
